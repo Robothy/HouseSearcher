596b2f3193625517a166d5e273ced98a093a5b95
robothy <robothyluo@gmail.com>
2017-04-13 21:57:18 +0800

    新增住房比较页面

---
+++ WebContent/js/LoadCompareHouses.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ * 加载待比较房屋到比较页面 compare-houses.jsp 的 js 代码
+ * 
+ */
+
+$(document).ready(function() {
+	var housesCookie = $.cookie("compareHouses");
+	if(housesCookie==undefined || housesCookie==null || housesCookie==""){	//cookie为空
+		alert("请先添加待比较的房屋信息");
+		window.location.href = "/HouseSearcher/index.jsp";	//跳转到首页
+	}
+	
+	var houses = housesCookie.split('-$-');
+	var context = "";
+	for(var i=0; i<houses.length; i++){
+		var house = JSON.parse(houses[i]);
+		context += "<tr>";
+		context += "<td>" + i + "</td>";
+		context += "<td><a href=\""+ house["houseHref"] +"\">" + house["title"] + "</a></td>";
+		context += "<td>" + house["price"] + "</td>";
+		context += "<td>" + house["area"] + "</td>";
+		context += "<td>" + house["houseType"] + "</td>";
+		context += "<td>" + house["level"] + "</td>";
+		context += "<td>" + house["orientation"] + "</td>";
+		context += "<td>" + house["pubDate"] + "</td>";
+		context += "<td>" + house["community"] + "</td>";
+		context += "<td>" + house["addr"] + "</td>";
+		context += "<td>" + "80" + "</td>";
+		context += "<td><span class=\"del-compare-house\" title=\"移除\" style=\"cursor: pointer; cursor: hand; display:inline-block; border:solid gray 1px; margin-right:0px; font-size:11px; \">X</span></td>";
+		context += "</tr>"
+	}
+	
+	$("#house-context").html(context);
+	
+})
---
+++ WebContent/pages/compare-houses.jsp
@@ -0,0 +1,52 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>住房对比</title>
+<%@ include file="module/references.txt" %>
+<script type="text/javascript" src="/HouseSearcher/js/LoadCompareHouses.js"></script>
+</head>
+<body>
+	<div class="container">
+  <h2>住房比较</h2>
+  <hr style="border-width: 2px; border-color:#39AC6A; "/>                        
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>编号</th>
+        <th>标题</th>
+        <th>价格</th>
+        <th>面积</th>
+        <th>户型</th>
+        <th>楼层</th>
+        <th>朝向</th>
+        <th>上架日期</th>
+        <th>小区</th>
+        <th>地址</th>
+        <th>评分</th>
+        <th>取消对比</th>
+      </tr>
+    </thead>
+    <tbody id="house-context">
+      <tr>
+        <td>1</td>
+        <td>好房子</td>
+        <td>2000元/月</td>
+        <td>20平</td>
+        <td>4室2厅</td>
+        <td>12层</td>
+        <td>朝北</td>
+        <td>2017-03-05</td>
+        <td>杨浦区</td>
+        <td>军工路516好</td>
+        <td>80</td>
+        <td><span>X</span></td>
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+</body>
+</html>

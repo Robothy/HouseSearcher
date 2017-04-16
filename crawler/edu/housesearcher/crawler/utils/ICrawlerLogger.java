3a78b8451883d5e0b844be3166558630f7b4ffe3
robothy <robothyluo@gmail.com>
2017-04-16 09:25:54 +0800

    nothing

--- crawler/edu/housesearcher/crawler/utils/ICrawlerLogger.java
+++ crawler/edu/housesearcher/crawler/utils/ICrawlerLogger.java
@@ -12,4 +12,30 @@ import org.apache.log4j.Logger;
  */
 public interface ICrawlerLogger extends Serializable {
     public static final Logger CRAWLER_LOGGER = LogManager.getLogger("crawlerLogger");
+    
+    public static void debug(Object message){
+	LogManager.getLogger("crawlerLogger").debug(message);
+    }
+    
+    public static void debug(Object message,Throwable throwable){
+	LogManager.getLogger("crawlerLogger").debug(message,throwable);
+    }
+    
+    public static void info(Object message){
+	LogManager.getLogger("crawlerLogger").info(message);
+    }
+    
+    public static void info(Object message,Throwable throwable){
+	LogManager.getLogger("crawlerLogger").info(message,throwable);
+    }
+    
+    public static void error(Object message){
+	LogManager.getLogger("crawlerLogger").error(message);
+    }
+    
+    public static void error(Object message,Throwable throwable){
+	LogManager.getLogger("crawlerLogger").error(message,throwable);
+    }
+    
+    
 }

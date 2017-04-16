7499a9f674d7f9d064afa95f4ac86130fd7d2cbe
robothy <robothyluo@gmail.com>
2017-04-16 08:23:34 +0800

    修改HouseListElementDao.java ， 新增批量获取房屋列表元素的方法。

--- src/edu/housesearcher/web/dao/HouseListElementDao.java
+++ src/edu/housesearcher/web/dao/HouseListElementDao.java
@@ -1,5 +1,8 @@
 package edu.housesearcher.web.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.housesearcher.crawler.entity.EntHouse;
 import edu.housesearcher.web.beans.HouseListElement;
 
@@ -8,4 +11,13 @@ public class HouseListElementDao {
     HouseListElement getHouseListElementByEntHouse(EntHouse house){
 	return new HouseListElement(house);
     }
+    
+    List<HouseListElement> getHouseListElementsByEntHouses(List<EntHouse> houses){
+	List<HouseListElement> result = new ArrayList<>();
+	for(EntHouse h: houses){
+	    result.add(new HouseListElement(h));
+	}
+	return result;
+    }
+    
 }
